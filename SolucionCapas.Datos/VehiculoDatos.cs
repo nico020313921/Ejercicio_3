78fc695bea17f19caa656d7da958e5a5ee82742a
nico020313921 <veignic09@gmail.com>
2026-09-22 12:34:33 -0300

    Add files via upload

---
+++ SolucionCapas.Datos/VehiculoDatos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace SolucionCapas.Datos
+{
+    public class VehiculoDatos
+    {
+        private string _conexionString = "Server=localhost;Database=estacionamiento;Uid=root;Pwd=;";
+
+        public (string Patente, string Modelo, bool TieneDeuda)? BuscarPorPatente(string patente)
+        {
+            string query = "SELECT Patente, Modelo, TieneDeuda FROM vehiculos WHERE Patente = @Patente";
+
+            using (MySqlConnection conexion = new MySqlConnection(_conexionString))
+            {
+                MySqlCommand comando = new MySqlCommand(query, conexion);
+                comando.Parameters.AddWithValue("@Patente", patente);
+
+                conexion.Open();
+
+                using (MySqlDataReader reader = comando.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        string patenteDb = reader["Patente"].ToString();
+                        string modeloDb = reader["Modelo"].ToString();
+                        bool tieneDeudaDb = Convert.ToBoolean(reader["TieneDeuda"]);
+
+                        return (patenteDb, modeloDb, tieneDeudaDb);
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
